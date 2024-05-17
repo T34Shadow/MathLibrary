727fa0a1f5ae6f281e12d988d0514e93fc2121b8
T34Shadow <132462529+T34Shadow@users.noreply.github.com>
2024-05-17 10:36:37 +1000

    Colour lib completed
    all of the colour test result passed

--- MathLibraryTests - STUDENT/MathHeaders/Colour.cpp
+++ MathLibraryTests - STUDENT/MathHeaders/Colour.cpp
@@ -17,48 +17,77 @@ MathClasses::Colour::Colour()
 
 MathClasses::Colour::Colour(Byte red, Byte green, Byte blue, Byte alpha)
 {
-    colour = (red << 24) | (blue << 16) | (green << 8) | alpha;
+    colour = (red << 24) | (green << 16) | (blue << 8) | alpha;
 }
 
 Byte MathClasses::Colour::GetRed() const
 {
-    return Byte();
+    return colour >> 24;
 }
 
 Byte MathClasses::Colour::GetGreen() const
 {
-    return Byte();
+    return colour >> 16;
 }
 
 Byte MathClasses::Colour::GetBlue() const
 {
-    return Byte();
+    return colour >> 8;
 }
+
+
 
 Byte MathClasses::Colour::GetAlpha() const
 {
-    return Byte();
+    return colour;
 }
 
-void MathClasses::Colour::SetRed(Byte v)
+void MathClasses::Colour::SetRed(Byte red)
 {
+    unsigned int v = (unsigned int)red << 24;
+
+    colour = colour & 0x00FFFFFF;
+    
+    colour = colour | v;
 }
 
-void MathClasses::Colour::SetGreen(Byte v)
+void MathClasses::Colour::SetGreen(Byte green)
 {
+    unsigned int v = (unsigned int)green << 16;
+
+    colour = colour & 0xFF00FFFF;
+
+    colour = colour | v;
 }
 
-void MathClasses::Colour::SetBlue(Byte v)
+void MathClasses::Colour::SetBlue(Byte blue)
 {
+    unsigned int v = (unsigned int)blue << 8;
+
+    colour = colour & 0xFFFF00FF;
+
+    colour = colour | v;
 }
 
-void MathClasses::Colour::SetAlpha(Byte v)
+void MathClasses::Colour::SetAlpha(Byte alpha)
 {
+    unsigned int v = (unsigned int)alpha;
+
+    colour = colour & 0xFFFFFF00;
+
+    colour = colour | v;
 }
 
 bool MathClasses::operator==(Colour a, Colour b)
 {
-    return false;
+    if (a.colour == b.colour)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 bool MathClasses::operator!=(Colour a, Colour b)
